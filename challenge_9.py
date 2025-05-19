d05da6d4ce58ffa59069bb7f78872627ae6278f8
GabrielF4 <112709330+GabrielF4@users.noreply.github.com>
2025-05-19 21:11:55 +0200

    Added new challenges

---
+++ challenge_9.py
@@ -0,0 +1,14 @@
+def all_nines(x):
+
+    if x % 2 == 0 or x % 5 == 0:
+        return -1
+    
+    for i in range(1, 4000):
+        num_str = str('9' * i)
+        num = int(num_str)
+        if num % x == 0:
+            return num // x
+    
+    return -1
+
+print(all_nines(3989))
--- hands_challenge.py
+++ hands_challenge.py
@@ -1,2 +1,5 @@
 def get_positions(n):
     return (n % 3, n % 9 // 3, n % 27 // 9)
+
+if __name__ == "__main__":
+    print(get_positions(5))
